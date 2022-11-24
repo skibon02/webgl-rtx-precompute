763a39ca92f8cd5cefd7782a317227c2dd5aa126
Vladislav Skiba <skibav2002@gmail.com>
2022-11-24 18:09:50 +0300

    rte almost done. Need implement diffuse to light calculation

---
+++ main.js
@@ -0,0 +1,197 @@
+let gl;
+class Prog {
+    constructor(name) {
+        this.name = name;
+        this.init = false;
+    }
+    createShader( type, source) {
+        var shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+        if (success) {
+            return shader;
+        }
+
+        console.log(gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+    }
+    createProgram( vertexShader, fragmentShader) {
+        var program = gl.createProgram();
+        gl.attachShader(program, vertexShader);
+        gl.attachShader(program, fragmentShader);
+        gl.linkProgram(program);
+        var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+        if (success) {
+            return program;
+        }
+
+        console.log(gl.getProgramInfoLog(program));
+        gl.deleteProgram(program);
+    }
+
+    u_(loc) {
+        return gl.getUniformLocation(this.program, "u_" + loc);
+    }
+    async initProgram() {
+        var vertexShader = this.createShader(gl.VERTEX_SHADER, await fetch("shaders/" + this.name + "_vert.glsl").then(r=> r.text()));
+        var fragmentShader = this.createShader(gl.FRAGMENT_SHADER, await fetch("shaders/" + this.name + "_frag.glsl").then(r=> r.text()));
+        this.program = this.createProgram(vertexShader, fragmentShader);
+        this.vao = gl.createVertexArray();
+
+        gl.useProgram(this.program);
+        gl.bindVertexArray(this.vao);
+
+        this.bgQuad = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.bgQuad);
+        let positions = [
+            -1.0,  1.0,
+            1.0,  1.0,
+            -1.0, -1.0,
+            -1.0, -1.0,
+            1.0,  1.0,
+            1.0, -1.0,
+        ];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+        gl.enableVertexAttribArray(gl.getAttribLocation(this.program, "a_position"));
+        gl.vertexAttribPointer(gl.getAttribLocation(this.program, "a_position"), 2, gl.FLOAT, true, 0, 0);
+        this.init = true;
+    }
+
+    prepareDraw() {
+        if(!this.init) {
+            throw new Error("Graphics not initialized");
+        }
+        gl.useProgram(this.program);
+        gl.bindVertexArray(this.vao);
+
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+    }
+}
+
+
+class App {
+    constructor() {
+        this.programs = [];
+        this.initGraphics();
+    }
+    async initGraphics() {
+        let canvas = document.querySelector("#c");
+        this.keydownHandler = this.keydown.bind(this);
+        this.keyupHandler = this.keyup.bind(this);
+        window.addEventListener('keydown',this.keydownHandler,false);
+        window.addEventListener('keyup',this.keyupHandler,false);
+       
+    
+        gl = canvas.getContext("webgl2");
+        // get required extensions for luminance textures
+        if (!gl.getExtension('EXT_color_buffer_float')) {
+            return alert('need EXT_color_buffer_float');
+        }
+        if (!gl.getExtension('OES_texture_float_linear')) {
+            return alert('need OES_texture_float_linear');
+        }
+
+        if (!gl) {
+            return alert('No webGL :(');
+        }
+            
+        
+        // gl.enable(gl.BLEND);
+        // gl.blendFunc(gl.ONE, gl.ONE);
+
+        //init shaders
+        let editor_prog = new Prog("game");
+        await editor_prog.initProgram();
+        gl.uniform2f(editor_prog.u_("resolution"), gl.canvas.width, gl.canvas.height);
+        this.programs.push(editor_prog);
+        
+        //setup the viewport
+        gl.viewport(0, 0, canvas.width, canvas.height);
+
+        this.resizeObserver = new ResizeObserver(this.resizeCanvasToDisplaySize.bind(this));
+        this.resizeObserver.observe(canvas);
+        this.resizeCanvasToDisplaySize([{target: canvas}]);
+        //start rendering cycle
+        window.requestAnimationFrame(this.draw.bind(this));
+    }
+    keyup(e){
+        if(e.key == " "){
+            e.preventDefault();
+            //this.clearAfterPass = !this.clearAfterPass;
+        }
+    }
+    keydown(e){
+        if(e.key == " "){
+            e.preventDefault();
+        }
+    }
+
+
+    resizeCanvasToDisplaySize(entries) {
+        if(this.stopped)
+            return;
+        for(let entry of entries) {
+            let canvas = entry.target;
+            // Lookup the size the browser is displaying the canvas in CSS pixels.
+            const dpr = window.devicePixelRatio;
+            const displayWidth  = Math.round(canvas.clientWidth * dpr);
+            const displayHeight = Math.round(canvas.clientHeight * dpr);
+
+            // Check if the canvas is not the same size.
+            const needResize = canvas.width  !== displayWidth || 
+                                canvas.height !== displayHeight;
+
+            if (needResize) {
+                // Make the canvas the same size
+
+                canvas.width  = displayWidth;
+                canvas.height = displayHeight;
+                gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+                this.notifyResize();
+            }
+        }
+    }
+
+    notifyResize() {
+        gl.uniform2f(this.programs[0].u_("resolution"), gl.canvas.width, gl.canvas.height);
+        // console.log('resized');
+    }
+    draw(timestamp) {  
+        this.lastTimestamp = timestamp;
+        if(this.stopped) {
+            return;
+        }
+
+        let program = this.programs[0]
+        program.prepareDraw();
+        gl.uniform1f(this.programs[0].u_("seed"), Math.random()*timestamp * 1.623426 % 1);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+        // setTimeout(() => {
+        window.requestAnimationFrame(this.draw.bind(this));
+        // }, 60);
+    }
+
+    cleanup() {
+        this.stopped = true;
+        this.resizeObserver.disconnect();
+
+        window.removeEventListener('keydown', this.keydownHandler);
+        window.removeEventListener('keyup', this.keyupHandler);
+
+        // this.gl.deleteTexture(this.bgtexture);
+        // this.gl.deleteBuffer(this.vertexBuffer);
+
+        // this.gl.deleteProgram(this.program);
+        // this.gl.deleteVertexArray(this.vao);
+        //this.gl.deleteVertexArray(this.anime_vao);
+
+    }
+
+}
+
+let app = new App();
---
+++ shaders/game_frag.glsl
@@ -0,0 +1,355 @@
+#version 300 es
+
+precision highp float;
+
+in vec2 pos;
+
+uniform vec2 u_resolution;
+uniform float u_seed;
+
+const float PI = 3.1415926535897932384626433832795;
+const float eps = 1e-5;
+
+float cur_seed;
+
+out vec4 outColor;
+
+struct Material {
+    vec3 albedo;
+    vec3 emission;
+    float reflectivity;
+    float albedoFactor;
+    bool isGlass;
+};
+
+struct Sphere {
+    vec3 center;
+    float radius;
+    Material material;
+};
+
+struct Plane {
+    vec3 normal;
+    float distance;
+    Material material;
+};
+
+struct Cube {
+    vec3 min;
+    vec3 max;
+    Material material;
+};
+
+struct Intersection {
+    vec3 position;
+    vec3 normal;
+    float distance;
+
+    Material material;
+};
+
+struct Ray {
+    vec3 origin;
+    vec3 dir;
+};
+
+float random(vec3 scale, float seed) {
+    return fract(sin(dot(gl_FragCoord.xyz + seed, scale)) * 43758.5453 + seed);
+}
+
+//Scene
+const int numSpheres = 4;
+Sphere spheres[numSpheres] = Sphere[](
+    //metal
+    Sphere(vec3(-0.75, -1.45, -4.4), 1.05, 
+    Material(
+        vec3(0.8, 0.4, 0.8), 
+        vec3(0.0), 1.0, 0.8, false)),
+
+    //glass
+    Sphere(vec3(2.0, -2.05, -3.7), 0.5, 
+    Material(
+        vec3(0.9, 1.0, 0.8), 
+        vec3(0.0), 0.0, 0.8, true)),
+
+    Sphere(vec3(-1.75, -1.95, -3.1), 0.6, 
+    Material(
+        vec3(1, 1, 1), 
+        vec3(0.0), 0.0, 0.8, false)),
+
+    //light
+    Sphere(vec3(0, 17.8, -1), 15.0, 
+        Material(
+            vec3(0.0, 0.0, 0.0), 
+            vec3(50000.0, 40000.0, 45000.0), 0.0, 0.8, false))
+);
+
+const int numPlanes = 6;
+Plane planes[numPlanes] = Plane[](
+    Plane(vec3(0, 1, 0), 2.5, 
+        Material(
+            vec3(0.9, 0.9, 0.9), 
+            vec3(0.0), 0.6, 0.8, false)),
+    Plane(vec3(0, -1, 0), 3.0,
+        Material(
+            vec3(0.9, 0.9, 0.9), 
+            vec3(0.0), 0.0, 0.8, false)),
+
+    //Left / Right
+    Plane(vec3(1, 0, 0), 2.75,
+        Material(
+            vec3(1, 0.1, 0.1), 
+            vec3(0.0), 0.4, 0.8, false)),
+    Plane(vec3(-1, 0, 0), 2.75,
+        Material(
+            vec3(0.1, 1, 0.1), 
+            vec3(0.0), 0.0, 0.8, false)),
+
+    //Back / Front
+    Plane(vec3(0, 0, 1), 6.0,
+        Material(
+            vec3(0.8, 0.8, 0.5), 
+            vec3(0.0), 1.0, 0.6, false)),
+    Plane(vec3(0, 0, -1), 0.5,
+        Material(
+            vec3(0.9, 0.9, 0.9), 
+            vec3(0.0), 0.0, 0.8, false))
+);
+
+const int numCubes = 1;
+Cube cubes[numCubes] = Cube[](
+    Cube(vec3(2.4, 1.0, -4.5), vec3(2.6, 1.8, -3.5),
+        Material(
+            vec3(0.6, 0.6, 0.9), 
+            vec3(0.0), 0.1, 0.7, true))
+);
+
+
+
+Intersection intersect(Ray ray) {
+    Intersection intersection;
+    intersection.distance = -1.0;
+
+    for (int i = 0; i < numSpheres; i++) {
+        Sphere sphere = spheres[i];
+
+        vec3 oc = ray.origin - sphere.center;
+        float b = dot(oc, ray.dir);
+        float c = dot(oc, oc) - sphere.radius * sphere.radius;
+        float h = b * b - c;
+
+        if (h >= 0.0) {
+            float h = sqrt(h);
+            float t = -b - h;
+            if(t < eps)
+                t = -b + h;
+            
+            if (t >= eps && (intersection.distance < 0.0 || t < intersection.distance)) {
+                intersection.distance = t;
+                intersection.position = ray.origin + ray.dir * t;
+                intersection.normal = normalize(intersection.position - sphere.center);
+                intersection.material = sphere.material;
+            }
+        }
+    }
+
+    for (int i = 0; i < numPlanes; i++) {
+        Plane plane = planes[i];
+
+        float denom = dot(ray.dir, plane.normal);
+        if (abs(denom) > 0.0001) {
+            float t = -(dot(ray.origin, plane.normal) + plane.distance) / denom;
+            if (t >= eps && (intersection.distance < 0.0 || t < intersection.distance)) {
+                intersection.distance = t;
+                intersection.position = ray.origin + ray.dir * t;
+                intersection.normal = plane.normal;
+                intersection.material = plane.material;
+            }
+        }
+    }
+
+    for (int i = 0; i < numCubes; i++) {
+        Cube cube = cubes[i];
+
+        vec3 invDir = 1.0 / ray.dir;
+        vec3 tbot = invDir * (cube.min - ray.origin);
+        vec3 ttop = invDir * (cube.max - ray.origin);
+
+        vec3 tmin = min(ttop, tbot);
+        vec3 tmax = max(ttop, tbot);
+
+        float t0 = max(max(tmin.x, tmin.y), tmin.z);
+        float t1 = min(min(tmax.x, tmax.y), tmax.z);
+
+        if (t0 < t1 && t1 >= eps) {
+            float t = t0;
+            if (t < eps)
+                t = t1;
+            if (t >= eps && (intersection.distance < 0.0 || t < intersection.distance)) {
+                intersection.distance = t;
+                intersection.position = ray.origin + ray.dir * t;
+                //calculate normal
+                vec3 n = vec3(0.0);
+                if (abs(intersection.position.x - cube.min.x) < eps)
+                    n.x = -1.0;
+                else if (abs(intersection.position.x - cube.max.x) < eps)
+                    n.x = 1.0;
+                else if (abs(intersection.position.y - cube.min.y) < eps)
+                    n.y = -1.0;
+                else if (abs(intersection.position.y - cube.max.y) < eps)
+                    n.y = 1.0;
+                else if (abs(intersection.position.z - cube.min.z) < eps)
+                    n.z = -1.0;
+                else if (abs(intersection.position.z - cube.max.z) < eps)
+                    n.z = 1.0;
+                intersection.normal = n;
+                intersection.material = cube.material;
+            }
+        }
+    }
+    return intersection;
+}
+
+vec3 addDirectLight(Intersection intersection) {
+    vec3 color = vec3(0.0);
+    for (int i = 0; i < numSpheres; i++) {
+        Sphere sphere = spheres[i];
+        if (sphere.material.emission.x > 0.0) {
+            vec3 lightDir = normalize(sphere.center + vec3(0.0, -15.2, 0.0) - intersection.position);
+            float lightDistance = length(sphere.center - intersection.position);
+            float lightAngle = dot(lightDir, intersection.normal); // cos
+            if (lightAngle > 0.0) {
+                float inclusionFactor = 0.15;
+                color += sphere.material.emission * lightAngle * inclusionFactor;
+                Ray shadowRay = Ray(intersection.position, lightDir);
+                Intersection shadowIntersection = intersect(shadowRay);
+                if (shadowIntersection.distance < 0.0 || shadowIntersection.distance > lightDistance) {
+                    color = sphere.material.emission * lightAngle;
+                }
+            }
+        }
+    }
+    return color;
+}
+
+const float finalLumScale = 0.0008;
+const int MAX_BOUNCES = 15;
+bool hitGlass = false;
+int hitGlassI = 0;
+int decision = 0;
+vec3 pathTrace(Ray ray) {
+    int depth = 0;
+    //color of ray, that flew out of the camera
+    vec3 lightColor = vec3(0.0);
+    vec3 resColor = vec3(0.0);
+    vec3 throughput = vec3(1.0);
+
+    while(depth < MAX_BOUNCES) {
+        Intersection intersection = intersect(ray);
+        if(intersection.distance == -1.0) {
+            break;
+        }
+
+        ray.origin = intersection.position;
+        
+        //update light color and throughput
+        if(intersection.material.emission != vec3(0.0)) {
+            lightColor = intersection.material.emission;
+            break;
+        } else {
+            if(intersection.material.isGlass) {
+                float n = 1.5;
+                float R0 = (1.0 - n) / (1.0 + n);
+                R0 = R0 * R0;
+                if(dot(ray.dir, intersection.normal) > 0.0) {
+                    intersection.normal = -intersection.normal;
+                    n = 1.0 / n;
+                }
+                n = 1.0 / n;
+                float cost1 = (-dot(ray.dir, intersection.normal));
+                float cost2 = 1.0 - n * n * (1.0 - cost1 * cost1);
+                float R = R0 + (1.0 - R0) * pow(1.0 - cost1, 5.0); // Schlick's approximation
+                cur_seed += random(vec3(16.231, 132.52, 25.3215), cur_seed);
+                throughput *= intersection.material.albedo;
+                if(cost2 > 0.0) {
+                    if(hitGlassI > 1)
+                    {
+                        ray.dir = normalize(n * ray.dir + (n * cost1 - sqrt(cost2)) * intersection.normal);
+                    }
+                    else
+                        if(hitGlassI == 0 && decision % 2 == 0 || hitGlassI == 1 && decision / 2 == 0) {
+                            ray.dir = normalize(n * ray.dir + (n * cost1 - sqrt(cost2)) * intersection.normal);
+                            throughput *= (1.0 - R) * 2.0;
+                        }
+                        else {
+                            ray.dir = normalize(reflect(ray.dir, intersection.normal));
+                            throughput *= R * 2.0;
+                        }
+                }
+                else {
+                    ray.dir = normalize(reflect(ray.dir, intersection.normal));
+                }
+                hitGlass = true;
+                hitGlassI++;
+            } 
+            else {
+                // test normals
+                // lightColor = abs(intersection.normal) * 500.0;
+                // break;
+                float diffuseFactor = 1.0 - intersection.material.reflectivity;
+                float reflectFactor = intersection.material.reflectivity;
+                //diffuse
+                vec3 lightEmission = addDirectLight( intersection);
+
+
+                resColor += lightEmission * intersection.material.albedo * intersection.material.albedoFactor * throughput * diffuseFactor / PI;
+                if(reflectFactor == 0.0) {
+                    break;
+                }
+                
+                //reflection
+                float cost = dot(ray.dir, intersection.normal);
+                ray.dir = normalize(ray.dir - intersection.normal * cost * 2.0);
+                throughput *= intersection.material.albedo * intersection.material.albedoFactor * reflectFactor;
+            }
+        }
+
+        depth++;
+    }
+    return resColor + lightColor * throughput;
+}
+const int SAMPLES = 1;
+const float aa_factor = 0.0;
+void main() {
+    cur_seed = u_seed;
+    Ray ray;
+    float fovscale = 1.0;
+    if(u_resolution.y > u_resolution.x) {
+        fovscale *= u_resolution.y / u_resolution.x;
+    }
+    ray.dir = vec3(0.0, 0.0, -1.0) + vec3(pos.x*(u_resolution.x / u_resolution.y), pos.y, 0.0) * fovscale;
+    ray.dir = normalize(ray.dir);
+    ray.origin = vec3(0.0, 0.0, 0.0);
+    vec3 col = vec3(0.0);
+    int samples_n = SAMPLES;
+    for(int i = 0; i < samples_n; i++) {
+        decision = i;
+        hitGlassI = 0;
+        if(aa_factor > 0.0) {
+            ray.dir = vec3(0.0, 0.0, -1.0) + vec3(pos.x*(u_resolution.x / u_resolution.y), pos.y, 0.0) * fovscale;
+            ray.dir.x += (random(vec3(525.315, 126.26, 12.42), cur_seed + float(i)) - 0.5) / u_resolution.x * aa_factor;
+            ray.dir.y += (random(vec3(125.231, 162.135, 115.321), cur_seed + float(i)) - 0.5) / u_resolution.y * aa_factor;
+            ray.dir = normalize(ray.dir);
+        }
+        cur_seed += random(vec3(315.231, 13.5123, 125.3215), cur_seed + float(i));
+        col += pathTrace(ray);
+        if(hitGlass)
+            samples_n= 4;
+    }
+    col /= float(samples_n);
+    col *=  finalLumScale;
+    vec2 texCoord = pos * 0.5 + 0.5;
+    //vec4 oldCol = texture(u_prev_frame, texCoord);
+    outColor = vec4(col, 1.0);
+
+}
